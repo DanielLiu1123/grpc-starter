dcda3007df19ef9f4ef67347d69b3f16e29a6d38
Freeman Liu <llw599502537@gmail.com>
2023-05-23 23:09:46 +0800

    add grpc client starter tests

---
+++ grpc-starters/grpc-client-boot-starter/src/test/java/com/freemanan/starter/grpc/client/GrpcClientIT.java
@@ -0,0 +1,75 @@
+package com.freemanan.starter.grpc.client;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import com.freemanan.sample.pet.v1.PersonServiceGrpc;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Freeman
+ */
+class GrpcClientIT {
+
+    @Test
+    void testGrpcStubAutowired_whenNotConfigureBasePackages() {
+        ConfigurableApplicationContext ctx = new SpringApplicationBuilder(Cfg.class).run();
+
+        assertThatCode(() -> ctx.getBean(GrpcClientProperties.class)).doesNotThrowAnyException();
+        assertThatCode(() -> ctx.getBean(PersonServiceGrpc.PersonServiceBlockingStub.class))
+                .isInstanceOf(NoSuchBeanDefinitionException.class);
+
+        ctx.close();
+    }
+
+    @Test
+    void testGrpcStubAutowired_whenNotConfigureAuthority() {
+        ConfigurableApplicationContext ctx = new SpringApplicationBuilder(Cfg.class)
+                .properties(GrpcClientProperties.PREFIX + ".base-packages[0]=com")
+                .run();
+
+        assertThatCode(() -> ctx.getBean(GrpcClientProperties.class)).doesNotThrowAnyException();
+        assertThatCode(() -> ctx.getBean(PersonServiceGrpc.PersonServiceBlockingStub.class))
+                .isInstanceOf(BeanCreationException.class)
+                .rootCause()
+                .hasMessageStartingWith("Not configure authority for stub");
+
+        ctx.close();
+    }
+
+    @Test
+    void testGrpcStubAutowired_whenOK() {
+        ConfigurableApplicationContext ctx = new SpringApplicationBuilder(Cfg.class)
+                .properties(GrpcClientProperties.PREFIX + ".base-packages[0]=com")
+                .properties(GrpcClientProperties.PREFIX + ".authority=localhost:8080")
+                .run();
+
+        assertThatCode(() -> ctx.getBean(GrpcClientProperties.class)).doesNotThrowAnyException();
+        assertThatCode(() -> ctx.getBean(PersonServiceGrpc.PersonServiceBlockingStub.class))
+                .doesNotThrowAnyException();
+
+        ctx.close();
+    }
+
+    @Test
+    void testGrpcStubAutowired_whenDisableGrpcClient() {
+        ConfigurableApplicationContext ctx = new SpringApplicationBuilder(Cfg.class)
+                .properties(GrpcClientProperties.PREFIX + ".enabled=false")
+                .run();
+
+        assertThatCode(() -> ctx.getBean(GrpcClientProperties.class)).isInstanceOf(NoSuchBeanDefinitionException.class);
+        assertThatCode(() -> ctx.getBean(PersonServiceGrpc.PersonServiceBlockingStub.class))
+                .isInstanceOf(NoSuchBeanDefinitionException.class);
+
+        ctx.close();
+    }
+
+    @Configuration(proxyBeanMethods = false)
+    @EnableAutoConfiguration
+    static class Cfg {}
+}
