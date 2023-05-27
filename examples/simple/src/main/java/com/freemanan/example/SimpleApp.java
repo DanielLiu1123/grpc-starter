b5287bd2a301637ce1da646f56ba113a8621e98f
Freeman Liu <llw599502537@gmail.com>
2023-05-28 02:27:22 +0800

    update README

--- examples/simple/src/main/java/com/freemanan/example/SimpleApp.java
+++ examples/simple/src/main/java/com/freemanan/example/SimpleApp.java
@@ -1,10 +1,6 @@
 package com.freemanan.example;
 
-import static com.freemanan.foo.v1.api.FooServiceGrpc.FooServiceBlockingStub;
-import static com.freemanan.foo.v1.api.FooServiceGrpc.FooServiceStub;
-
 import com.freemanan.starter.grpc.client.EnableGrpcClients;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,15 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Freeman
  */
 @SpringBootApplication
-@EnableGrpcClients(clients = {FooServiceBlockingStub.class, FooServiceStub.class})
+@EnableGrpcClients({"com.freemanan", "io.grpc"})
 public class SimpleApp {
     public static void main(String[] args) {
         SpringApplication.run(SimpleApp.class, args);
     }
-
-    @Autowired
-    private FooServiceBlockingStub fooBlockingStub;
-
-    @Autowired
-    private FooServiceStub fooStub;
 }
--- examples/simple/src/test/java/com/freemanan/example/SimpleAppTest.java
+++ examples/simple/src/test/java/com/freemanan/example/SimpleAppTest.java
@@ -6,22 +6,24 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import com.freemanan.foo.v1.api.Foo;
 import io.grpc.StatusRuntimeException;
+import io.grpc.testing.protobuf.SimpleRequest;
+import io.grpc.testing.protobuf.SimpleServiceGrpc;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(
-        properties = {
-            "grpc.server.in-process.name=test",
-            "grpc.client.in-process.name=test",
-        })
+@SpringBootTest
 class SimpleAppTest {
 
     @Autowired
-    private FooServiceBlockingStub fooBlockingStub;
+    FooServiceBlockingStub fooBlockingStub;
+
+    @Resource
+    SimpleServiceGrpc.SimpleServiceBlockingStub simpleServiceBlockingStub;
 
     @Test
-    void testInProcessTransport() {
+    void testCreateFoo() {
         Foo foo = fooBlockingStub.create(
                 Foo.newBuilder().setId("001").setName("Freeman").build());
         assertThat(foo.getId()).isEqualTo("001");
@@ -29,10 +31,18 @@ class SimpleAppTest {
     }
 
     @Test
-    void testBehavior_whenNoExceptionHandler() {
+    void testCreateFoo_whenServerErrorAndNoExceptionHandler_thenClientSideShouldGetUnknownCode() {
+        Foo foo = Foo.newBuilder().setId("002").setName("Fre").build();
         assertThatExceptionOfType(StatusRuntimeException.class)
-                .isThrownBy(() -> fooBlockingStub.create(
-                        Foo.newBuilder().setId("002").setName("Fre").build()))
+                .isThrownBy(() -> fooBlockingStub.create(foo))
                 .withMessageContaining("UNKNOWN");
+    }
+
+    @Test
+    void testUnaryRpc() {
+        String responseMessage = simpleServiceBlockingStub
+                .unaryRpc(SimpleRequest.newBuilder().setRequestMessage("Hello").build())
+                .getResponseMessage();
+        assertThat(responseMessage).isEqualTo("Hi, I got your message: Hello");
     }
 }
