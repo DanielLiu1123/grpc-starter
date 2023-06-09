b4738957d5de2814060365906f42e1ea4844c8c6
Freeman Liu <llw599502537@gmail.com>
2023-06-09 22:05:14 +0800

    refactor health

--- grpc-boot-autoconfigure/grpc-server-boot-autoconfigure/src/main/java/com/freemanan/starter/grpc/server/feature/health/HealthImpl.java
+++ grpc-boot-autoconfigure/grpc-server-boot-autoconfigure/src/main/java/com/freemanan/starter/grpc/server/feature/health/HealthImpl.java
@@ -2,11 +2,15 @@ package com.freemanan.starter.grpc.server.feature.health;
 
 import static io.grpc.health.v1.HealthCheckResponse.ServingStatus;
 
+import io.grpc.Status;
+import io.grpc.StatusException;
 import io.grpc.health.v1.HealthCheckRequest;
 import io.grpc.health.v1.HealthCheckResponse;
 import io.grpc.health.v1.HealthGrpc;
 import io.grpc.stub.StreamObserver;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,47 +18,57 @@ import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.util.StringUtils;
 
 /**
- * TODO(Freeman): need refactor, see HealthServiceImpl
- *
  * @author Freeman
  */
 public class HealthImpl extends HealthGrpc.HealthImplBase {
     private static final Logger log = LoggerFactory.getLogger(HealthImpl.class);
 
-    private final List<HealthChecker> healthCheckers;
+    /**
+     * service name to {@link HealthChecker}.
+     */
+    private final Map<String, HealthChecker> serviceToChecker;
 
     public HealthImpl(ObjectProvider<HealthChecker> healthCheckers) {
-        this.healthCheckers = healthCheckers.orderedStream().collect(Collectors.toList());
+        this.serviceToChecker = healthCheckers
+                .orderedStream()
+                .collect(Collectors.toMap(
+                        HealthChecker::service, Function.identity(), (oldV, newV) -> oldV, LinkedHashMap::new));
     }
 
     @Override
     public void check(HealthCheckRequest request, StreamObserver<HealthCheckResponse> so) {
-        List<HealthChecker> checkers = getCheckers(request.getService());
+        String service = request.getService();
 
-        boolean healthy = true;
-        for (HealthChecker healthChecker : checkers) {
-            if (!healthChecker.check()) {
-                healthy = false;
-                log.warn("Health check failed: {}", healthChecker.getClass().getSimpleName());
-                break;
+        if (StringUtils.hasText(service)) {
+            HealthChecker checker = serviceToChecker.get(service);
+            if (checker == null) {
+                // see io.grpc.protobuf.services.HealthServiceImpl#check
+                so.onError(new StatusException(Status.NOT_FOUND.withDescription("unknown service " + service)));
+            } else {
+                boolean healthy = checker.check();
+                ServingStatus status = healthy ? ServingStatus.SERVING : ServingStatus.NOT_SERVING;
+                so.onNext(HealthCheckResponse.newBuilder().setStatus(status).build());
+                so.onCompleted();
             }
+        } else {
+            boolean healthy = true;
+            for (Map.Entry<String, HealthChecker> en : serviceToChecker.entrySet()) {
+                HealthChecker checker = en.getValue();
+                if (!checker.check()) {
+                    healthy = false;
+                    log.warn("Health check failed: {}", checker.getClass().getSimpleName());
+                    break;
+                }
+            }
+            ServingStatus status = healthy ? ServingStatus.SERVING : ServingStatus.NOT_SERVING;
+            so.onNext(HealthCheckResponse.newBuilder().setStatus(status).build());
+            so.onCompleted();
         }
-        ServingStatus status = healthy ? ServingStatus.SERVING : ServingStatus.NOT_SERVING;
-        so.onNext(HealthCheckResponse.newBuilder().setStatus(status).build());
-        so.onCompleted();
     }
 
     @Override
     public void watch(HealthCheckRequest request, StreamObserver<HealthCheckResponse> responseObserver) {
         // TODO(Freeman): need to implement it?
         super.watch(request, responseObserver);
-    }
-
-    protected List<HealthChecker> getCheckers(String service) {
-        return StringUtils.hasText(service)
-                ? healthCheckers.stream()
-                        .filter(healthChecker -> service.equalsIgnoreCase(healthChecker.service()))
-                        .collect(Collectors.toList())
-                : healthCheckers;
     }
 }
---
+++ grpc-boot-autoconfigure/grpc-server-boot-autoconfigure/src/test/java/com/freemanan/starter/grpc/server/HealthIT.java
@@ -0,0 +1,88 @@
+package com.freemanan.starter.grpc.server;
+
+import static io.grpc.health.v1.HealthCheckResponse.ServingStatus;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import com.freemanan.starter.grpc.server.feature.health.HealthChecker;
+import io.grpc.ManagedChannel;
+import io.grpc.StatusRuntimeException;
+import io.grpc.health.v1.HealthCheckRequest;
+import io.grpc.health.v1.HealthCheckResponse;
+import io.grpc.health.v1.HealthGrpc;
+import io.grpc.inprocess.InProcessChannelBuilder;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Freeman
+ */
+class HealthIT {
+
+    @Test
+    void testExternalHealthChecker() {
+        String name = UUID.randomUUID().toString();
+        ConfigurableApplicationContext ctx = new SpringApplicationBuilder(Cfg.class)
+                .properties(GrpcServerProperties.InProcess.PREFIX + ".name=" + name)
+                .run();
+
+        List<HealthChecker> checkers =
+                ctx.getBeanProvider(HealthChecker.class).orderedStream().collect(Collectors.toList());
+
+        assertThat(checkers).hasSize(1);
+        assertThat(checkers.get(0).service()).isEqualTo("foo");
+
+        ManagedChannel chan =
+                InProcessChannelBuilder.forName(name).directExecutor().build();
+        HealthGrpc.HealthBlockingStub stub = HealthGrpc.newBlockingStub(chan);
+
+        // check existing service
+        HealthCheckResponse resp =
+                stub.check(HealthCheckRequest.newBuilder().setService("foo").build());
+        assertThat(resp.getStatus()).isEqualTo(ServingStatus.SERVING);
+
+        // check unknown service
+        HealthCheckRequest req1 =
+                HealthCheckRequest.newBuilder().setService("bar").build();
+        assertThatExceptionOfType(StatusRuntimeException.class)
+                .isThrownBy(() -> stub.check(req1))
+                .withMessage("NOT_FOUND: unknown service bar");
+
+        // service is case-sensitive
+        HealthCheckRequest req2 =
+                HealthCheckRequest.newBuilder().setService("Foo").build();
+        assertThatExceptionOfType(StatusRuntimeException.class)
+                .isThrownBy(() -> stub.check(req2))
+                .withMessage("NOT_FOUND: unknown service Foo");
+
+        chan.shutdown();
+        ctx.close();
+    }
+
+    @Configuration(proxyBeanMethods = false)
+    @EnableAutoConfiguration
+    static class Cfg {
+
+        @Bean
+        HealthChecker fooHealthChecker() {
+            return new HealthChecker() {
+                @Override
+                public String service() {
+                    return "foo";
+                }
+
+                @Override
+                public boolean check() {
+                    return true;
+                }
+            };
+        }
+    }
+}
