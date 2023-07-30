8fc3e0da216b09758d3b78da016d4527d679986b
Freeman Liu <llw599502537@gmail.com>
2023-07-30 18:10:32 +0800

    test add configuration

--- grpc-extensions/grpc-test/src/main/java/com/freemanan/starter/grpc/extensions/test/GrpcTestAutoConfiguration.java
+++ grpc-extensions/grpc-test/src/main/java/com/freemanan/starter/grpc/extensions/test/GrpcTestAutoConfiguration.java
@@ -2,6 +2,8 @@ package com.freemanan.starter.grpc.extensions.test;
 
 import com.freemanan.starter.grpc.server.GrpcServerStartedEvent;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +12,12 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnClass(GrpcServerStartedEvent.class) // server starter must be in classpath
+@ConditionalOnProperty(prefix = GrpcTestProperties.PREFIX, name = "enabled", matchIfMissing = true)
+@EnableConfigurationProperties(GrpcTestProperties.class)
 public class GrpcTestAutoConfiguration {
 
     @Bean
-    public static GrpcPortBeanPostProcessor grpcPortBeanPostProcessor() {
+    static GrpcPortBeanPostProcessor grpcPortBeanPostProcessor() {
         return new GrpcPortBeanPostProcessor();
     }
 }
--- grpc-extensions/grpc-test/src/main/java/com/freemanan/starter/grpc/extensions/test/GrpcTestEnvironmentPostProcessor.java
+++ grpc-extensions/grpc-test/src/main/java/com/freemanan/starter/grpc/extensions/test/GrpcTestEnvironmentPostProcessor.java
@@ -5,7 +5,10 @@ import com.freemanan.starter.grpc.server.GrpcServerProperties;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.annotation.AnnotationUtils;
@@ -20,6 +23,7 @@ import org.springframework.util.ClassUtils;
  * @author Freeman
  */
 public class GrpcTestEnvironmentPostProcessor implements EnvironmentPostProcessor {
+    private static final Logger log = LoggerFactory.getLogger(GrpcTestEnvironmentPostProcessor.class);
 
     private static final boolean SPRING_BOOT_TEST_ENV =
             ClassUtils.isPresent("org.springframework.boot.test.context.SpringBootTest", null);
@@ -40,30 +44,49 @@ public class GrpcTestEnvironmentPostProcessor implements EnvironmentPostProcesso
             return;
         }
 
-        Map<String, Object> configMap = new HashMap<>();
+        GrpcTestProperties properties = Binder.get(environment)
+                .bind(GrpcTestProperties.PREFIX, GrpcTestProperties.class)
+                .orElseGet(GrpcTestProperties::new);
 
-        startGrpcWithInProcessIfNecessary(environment, configMap);
+        GrpcTestProperties.Server.Port port = properties.getServer().getPort();
+
+        Map<String, Object> configMap = new HashMap<>();
+        switch (port) {
+            case IN_PROCESS:
+                if (GRPC_CLIENT_STARTER_EXISTS && GRPC_SERVER_STARTER_EXISTS) {
+                    String serverProperty = GrpcServerProperties.InProcess.PREFIX + ".name";
+                    String clientProperty = GrpcClientProperties.InProcess.PREFIX + ".name";
+                    if (!environment.containsProperty(serverProperty)
+                            && !environment.containsProperty(clientProperty)) {
+                        // use in-process if not manually configured
+                        String name = UUID.randomUUID().toString();
+                        configMap.put(serverProperty, name);
+                        configMap.put(clientProperty, name);
+                    }
+                } else if (GRPC_SERVER_STARTER_EXISTS) {
+                    String portProperty = GrpcServerProperties.PREFIX + ".port";
+                    configMap.put(portProperty, 0);
+                    if (log.isInfoEnabled()) {
+                        log.info(
+                                "Set 'grpc.test.server.port' to 'IN_PROCESS', but gRPC-client-starter not found in classpath, will start gRPC server with random port");
+                    }
+                } else {
+                    throw new IllegalStateException("gRPC client or server starter not found");
+                }
+                break;
+            case RANDOM_PORT:
+                String portProperty = GrpcServerProperties.PREFIX + ".port";
+                configMap.put(portProperty, 0);
+                break;
+            case DEFINED_PORT:
+                // do nothing
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown port type: " + port);
+        }
 
         MapPropertySource ps = new MapPropertySource("grpc.extensions.test.property_source", configMap);
 
         environment.getPropertySources().addAfter(StandardEnvironment.SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME, ps);
-    }
-
-    private static void startGrpcWithInProcessIfNecessary(
-            ConfigurableEnvironment environment, Map<String, Object> configMap) {
-        if (GRPC_CLIENT_STARTER_EXISTS && GRPC_SERVER_STARTER_EXISTS) {
-            String serverProperty = GrpcServerProperties.InProcess.PREFIX + ".name";
-            String clientProperty = GrpcClientProperties.InProcess.PREFIX + ".name";
-            if (!environment.containsProperty(serverProperty) && !environment.containsProperty(clientProperty)) {
-                // use in-process if not manually configured
-                String name = UUID.randomUUID().toString();
-                configMap.put(serverProperty, name);
-                configMap.put(clientProperty, name);
-            }
-        } else if (GRPC_SERVER_STARTER_EXISTS) {
-            // use random port
-            String portProperty = GrpcServerProperties.PREFIX + ".port";
-            configMap.put(portProperty, 0);
-        }
     }
 }
