814cd0f4337852fb9ae1223d7b62da86c6d7757a
Freeman Liu <llw599502537@gmail.com>
2023-10-12 00:24:36 +0800

    fix comment

--- grpc-extensions/grpc-test/src/main/java/com/freemanan/starter/grpc/extensions/test/InProcessName.java
+++ grpc-extensions/grpc-test/src/main/java/com/freemanan/starter/grpc/extensions/test/InProcessName.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annotation at the field that injects the gRPC server port that was allocated at runtime.
+ * Annotation at the field that injects the gRPC server in-process name.
  *
  * <p> Example:
  *
