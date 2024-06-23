fcb65f13f668496bbe01f734cc1a72ae0cd9288a
Freeman Liu <llw599502537@gmail.com>
2024-06-23 18:22:17 +0800

    Add transcoding best practice example

---
+++ examples/transcoding/best-practice/bp-server/src/main/java/grpcstarter/example/BestPracticeApp.java
@@ -0,0 +1,42 @@
+package grpcstarter.example;
+
+import io.grpc.stub.StreamObserver;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import user.v1.GetUserRequest;
+import user.v1.GetUserResponse;
+import user.v1.User;
+import user.v1.UserServiceGrpc;
+
+@SpringBootApplication
+public class BestPracticeApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestPracticeApp.class, args);
+    }
+
+
+    @Bean
+    UserServiceGrpc.UserServiceImplBase userServiceImpl() {
+        return new UserServiceGrpc.UserServiceImplBase() {
+            @Override
+            public void getUser(GetUserRequest request, StreamObserver<GetUserResponse> responseObserver) {
+                if (request.getId() < 10) {
+                    responseObserver.onNext(GetUserResponse.getDefaultInstance());
+                    responseObserver.onCompleted();
+                    return;
+                }
+
+                responseObserver.onNext(GetUserResponse.newBuilder()
+                        .setUser(User.newBuilder()
+                                .setId(request.getId())
+                                .setName("user-" + request.getId())
+                                .build())
+                        .build());
+                responseObserver.onCompleted();
+            }
+        };
+    }
+
+}
