378b8ded084860dca007f2ca3cbfafc516116e35
Freeman Liu <llw599502537@gmail.com>
2024-10-31 23:27:51 +0800

    native image support for transcoding (#65)
    * native image support for transcoding
    
    * fix tests

--- examples/transcoding/webflux/src/main/java/grpcstarter/example/TranscodingWebFluxApp.java
+++ examples/transcoding/webflux/src/main/java/grpcstarter/example/TranscodingWebFluxApp.java
@@ -8,18 +8,36 @@ import io.grpc.Metadata;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
+import java.util.Map;
+import java.util.Objects;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.reactive.context.ReactiveWebServerApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.util.Assert;
+import org.springframework.web.reactive.function.client.WebClient;
 
 /**
  * @author Freeman
  */
+@Slf4j
 @SpringBootApplication
 public class TranscodingWebFluxApp extends SimpleServiceImplBase {
 
     public static void main(String[] args) {
-        SpringApplication.run(TranscodingWebFluxApp.class, args);
+        var ctx = SpringApplication.run(TranscodingWebFluxApp.class, args);
+
+        // We need to do `./gradlew nativeRun` in CI, it needs to be closed.
+        // When running the example locally, no need to close it.
+        // See
+        // https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/store-information-in-variables#default-environment-variables
+        if (System.getenv("CI") != null) {
+            ctx.close();
+        }
     }
 
     @Override
@@ -46,5 +64,25 @@ public class TranscodingWebFluxApp extends SimpleServiceImplBase {
             Thread.sleep(1000);
         }
         r.onCompleted();
+    }
+
+    @Bean
+    ApplicationRunner runner(WebClient.Builder builder, ReactiveWebServerApplicationContext ctx) {
+        return args -> {
+            var client = builder.baseUrl(
+                            "http://localhost:" + ctx.getWebServer().getPort())
+                    .build();
+
+            var response = client.post()
+                    .uri("/unary")
+                    .bodyValue(Map.of("requestMessage", "World"))
+                    .retrieve()
+                    .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {})
+                    .block();
+            log.info("response: {}", response);
+
+            Assert.notNull(response, "response is null");
+            Assert.isTrue(Objects.equals(response.get("responseMessage"), "Hello World"), "response message not match");
+        };
     }
 }
---
+++ grpc-extensions/grpc-transcoding/src/main/java/grpcstarter/extensions/transcoding/GrpcTranscodingBeanFactoryInitializationAotProcessor.java
@@ -0,0 +1,153 @@
+package grpcstarter.extensions.transcoding;
+
+import com.google.protobuf.DescriptorProtos;
+import com.google.protobuf.Message;
+import io.grpc.BindableService;
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+import lombok.SneakyThrows;
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.ReflectionHints;
+import org.springframework.beans.factory.aot.BeanFactoryInitializationAotContribution;
+import org.springframework.beans.factory.aot.BeanFactoryInitializationAotProcessor;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.util.ClassUtils;
+
+/**
+ * TIP: use 'processAot' task to debug the AOT processing.
+ *
+ * @author Freeman
+ */
+class GrpcTranscodingBeanFactoryInitializationAotProcessor
+        implements BeanFactoryInitializationAotProcessor, EnvironmentAware {
+
+    private Environment env;
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.env = environment;
+    }
+
+    @Nullable
+    @Override
+    public BeanFactoryInitializationAotContribution processAheadOfTime(
+            @Nonnull ConfigurableListableBeanFactory beanFactory) {
+        return (generationContext, beanFactoryInitializationCode) -> {
+            var enabled = env.getProperty(GrpcTranscodingProperties.PREFIX + ".enabled", Boolean.class, true);
+            if (!enabled) {
+                return;
+            }
+
+            var reflection = generationContext.getRuntimeHints().reflection();
+
+            // See grpcstarter.extensions.transcoding.DefaultHeaderConverter.getHttpHeaders
+            reflection.registerType(HttpHeaders.class, builder -> builder.withMembers(MemberCategory.PUBLIC_FIELDS));
+
+            // This will increase the packaging size about 2MB.
+            // I don't know why this type is needed, don't want to spend much time to figure it out :)
+            registerReflectionForClassAndInnerClasses(reflection, DescriptorProtos.class);
+
+            // request + response messages
+            registerReflectionForMessages(reflection, getMessages(listGrpcServiceDefinition(beanFactory)));
+        };
+    }
+
+    private static void registerReflectionForClassAndInnerClasses(ReflectionHints reflection, Class<?> clz) {
+
+        reflection.registerType(clz, MemberCategory.INTROSPECT_PUBLIC_METHODS, MemberCategory.INVOKE_PUBLIC_METHODS);
+
+        for (var declaredClass : clz.getDeclaredClasses()) {
+            registerReflectionForClassAndInnerClasses(reflection, declaredClass);
+        }
+    }
+
+    private static LinkedHashSet<Class<?>> getMessages(Map<String, BeanDefinition> beanNameToBeanDefinition) {
+        var messages = new LinkedHashSet<Class<?>>();
+        for (var entry : beanNameToBeanDefinition.entrySet()) {
+            var beanDefinition = entry.getValue();
+            var clz = beanDefinition.getResolvableType().resolve();
+            if (clz == null) {
+                continue;
+            }
+
+            var methods = clz.getMethods();
+            for (var method : methods) {
+                Class<?> returnType = method.getReturnType();
+                if (returnType != void.class) { // grpc method should return void
+                    continue;
+                }
+                if (method.getParameterCount() != 2) { // grpc method should have 2 parameters
+                    continue;
+                }
+                Class<?> message1 = method.getParameterTypes()[0];
+                if (!Message.class.isAssignableFrom(message1)) { // the first parameter should be a Message
+                    continue;
+                }
+                Type arg2 = method.getGenericParameterTypes()[1]; // the second parameter must be a StreamObserver
+                if (!(arg2 instanceof ParameterizedType pt)) {
+                    continue;
+                }
+                var typeArgs = pt.getActualTypeArguments();
+                if (typeArgs.length != 1) {
+                    continue;
+                }
+                if (!(typeArgs[0] instanceof Class<?> message2)) {
+                    continue;
+                }
+                if (!Message.class.isAssignableFrom(message2)) { // the second parameter should be a Message
+                    continue;
+                }
+
+                messages.add(message1);
+                messages.add(message2);
+            }
+        }
+        return messages;
+    }
+
+    @SneakyThrows
+    private static void registerReflectionForMessages(ReflectionHints reflection, Set<Class<?>> messages) {
+        for (var message : messages) {
+
+            // register the message and its builder
+            reflection.registerType(
+                    message, MemberCategory.INTROSPECT_PUBLIC_METHODS, MemberCategory.INVOKE_PUBLIC_METHODS);
+
+            var builderClass = getBuilderClass(message);
+            if (builderClass != null) {
+                reflection.registerType(
+                        builderClass, MemberCategory.INTROSPECT_PUBLIC_METHODS, MemberCategory.INVOKE_PUBLIC_METHODS);
+            }
+        }
+    }
+
+    private static Class<?> getBuilderClass(Class<?> message) {
+        try {
+            return ClassUtils.forName(message.getName() + "$Builder", null);
+        } catch (ClassNotFoundException e) {
+            return null;
+        }
+    }
+
+    private static Map<String, BeanDefinition> listGrpcServiceDefinition(ConfigurableListableBeanFactory beanFactory) {
+        var beanDefinitions = new HashMap<String, BeanDefinition>();
+        for (String name : beanFactory.getBeanDefinitionNames()) {
+            BeanDefinition beanDefinition = beanFactory.getBeanDefinition(name);
+            Class<?> clz = beanDefinition.getResolvableType().resolve();
+            if (clz != null && BindableService.class.isAssignableFrom(clz)) {
+                beanDefinitions.put(name, beanDefinition);
+            }
+        }
+        return beanDefinitions;
+    }
+}
