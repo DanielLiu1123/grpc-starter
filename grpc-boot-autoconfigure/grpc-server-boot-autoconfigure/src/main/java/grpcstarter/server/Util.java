c439aeec1a138e026b3a3d93680009d9b434fc0c
Freeman Liu <llw599502537@gmail.com>
2024-12-28 23:21:21 +0800

    check if Reflection class exists
    (cherry picked from commit 3d5d5e793b561137d54d8e4485be7a9ec610bc22)

--- grpc-boot-autoconfigure/grpc-server-boot-autoconfigure/src/main/java/grpcstarter/server/Util.java
+++ grpc-boot-autoconfigure/grpc-server-boot-autoconfigure/src/main/java/grpcstarter/server/Util.java
@@ -3,7 +3,6 @@ package grpcstarter.server;
 import io.grpc.BindableService;
 import io.grpc.channelz.v1.ChannelzGrpc;
 import io.grpc.health.v1.HealthGrpc;
-import io.grpc.reflection.v1alpha.ServerReflectionGrpc;
 import java.util.HashSet;
 import java.util.Set;
 import lombok.experimental.UtilityClass;
@@ -15,11 +14,25 @@ import org.springframework.aop.framework.AopProxyUtils;
 @UtilityClass
 class Util {
 
-    private static final Set<Class<? extends BindableService>> internalServiceClasses = getInternalServiceClasses();
+    private static final Set<Class<?>> internalServiceClasses = getInternalServiceClasses();
 
-    private static Set<Class<? extends BindableService>> getInternalServiceClasses() {
-        Set<Class<? extends BindableService>> result = new HashSet<>();
-        result.add(ServerReflectionGrpc.ServerReflectionImplBase.class);
+    private static Set<Class<?>> getInternalServiceClasses() {
+        Set<Class<?>> result = new HashSet<>();
+
+        try {
+            // legacy Reflection
+            Class<?> clz = Class.forName("io.grpc.reflection.v1alpha.ServerReflectionGrpc$ServerReflectionImplBase");
+            result.add(clz);
+        } catch (ClassNotFoundException ignored) {
+        }
+
+        try {
+            // v1 Reflection
+            Class<?> clz = Class.forName("io.grpc.reflection.v1.ServerReflectionGrpc$ServerReflectionImplBase");
+            result.add(clz);
+        } catch (ClassNotFoundException ignored) {
+        }
+
         result.add(HealthGrpc.HealthImplBase.class);
         result.add(ChannelzGrpc.ChannelzImplBase.class);
         return result;
