4f6166c5866040fe906ef90991d7ebd4eba68a43
Freeman Liu <llw599502537@gmail.com>
2025-07-26 01:09:23 +0800

    chore: update Docusaurus dependencies to version 3.8.1 and set trailingSlash to false

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -17,6 +17,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'danielliu1123', // Usually your GitHub org/user name.
   projectName: 'grpc-starter', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
