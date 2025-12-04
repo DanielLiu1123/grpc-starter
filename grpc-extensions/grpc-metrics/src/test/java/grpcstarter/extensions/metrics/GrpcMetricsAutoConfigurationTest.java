a1ac1ed6bb7522a26084a96bef460e54fa3cec83
Freeman Liu <freeman@moego.pet>
2025-12-04 13:17:06 +0800

    Refactor gRPC metrics and tracing configuration to be built-in, removing unnecessary dependencies and simplifying tests

--- grpc-extensions/grpc-metrics/src/test/java/grpcstarter/extensions/metrics/GrpcMetricsAutoConfigurationTest.java
+++ grpc-extensions/grpc-metrics/src/test/java/grpcstarter/extensions/metrics/GrpcMetricsAutoConfigurationTest.java
@@ -15,7 +15,6 @@ import org.springframework.aop.config.AopConfigUtils;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -26,54 +25,50 @@ class GrpcMetricsAutoConfigurationTest {
     @Test
     @Classpath(add = "org.springframework.boot:spring-boot-starter-aspectj:" + SPRING_BOOT_VERSION)
     void testMetricsBeans_whenAllConditionsMatched() {
-        ConfigurableApplicationContext ctx = new SpringApplicationBuilder(Cfg.class)
+        try (var ctx = new SpringApplicationBuilder(Cfg.class)
                 .properties(GrpcServerProperties.PREFIX + ".in-process.name=" + UUID.randomUUID())
                 .properties("management.observations.annotations.enabled=true")
-                .run();
+                .run()) {
 
-        assertThatCode(() -> ctx.getBean(OrderedMetricCollectingServerInterceptor.class))
-                .doesNotThrowAnyException();
-        assertThatCode(() -> ctx.getBean(OrderedMetricCollectingClientInterceptor.class))
-                .doesNotThrowAnyException();
+            assertThatCode(() -> ctx.getBean(OrderedMetricCollectingServerInterceptor.class))
+                    .doesNotThrowAnyException();
+            assertThatCode(() -> ctx.getBean(OrderedMetricCollectingClientInterceptor.class))
+                    .doesNotThrowAnyException();
 
-        assertThatCode(() -> ctx.getBean(AopConfigUtils.AUTO_PROXY_CREATOR_BEAN_NAME))
-                .doesNotThrowAnyException();
-        assertThatCode(() -> ctx.getBean(AnnotationAwareAspectJAutoProxyCreator.class))
-                .doesNotThrowAnyException();
-        // MetricsAspectsAutoConfiguration add CountedAspect, TimedAspect and ObservedAspect beans from 3.2.0
-        assertThatCode(() -> ctx.getBean(CountedAspect.class)).doesNotThrowAnyException();
-        assertThatCode(() -> ctx.getBean(TimedAspect.class)).doesNotThrowAnyException();
-        assertThatCode(() -> ctx.getBean(ObservedAspect.class)).doesNotThrowAnyException();
-
-        ctx.close();
+            assertThatCode(() -> ctx.getBean(AopConfigUtils.AUTO_PROXY_CREATOR_BEAN_NAME))
+                    .doesNotThrowAnyException();
+            assertThatCode(() -> ctx.getBean(AnnotationAwareAspectJAutoProxyCreator.class))
+                    .doesNotThrowAnyException();
+            // MetricsAspectsAutoConfiguration add CountedAspect, TimedAspect and ObservedAspect beans from 3.2.0
+            assertThatCode(() -> ctx.getBean(CountedAspect.class)).doesNotThrowAnyException();
+            assertThatCode(() -> ctx.getBean(TimedAspect.class)).doesNotThrowAnyException();
+            assertThatCode(() -> ctx.getBean(ObservedAspect.class)).doesNotThrowAnyException();
+        }
     }
 
     @Test
     @Classpath(exclude = "grpc-server-boot-autoconfigure-*.jar")
     void testMetricsBeans_whenGrpcServerNotOnClasspath() {
-        ConfigurableApplicationContext ctx = new SpringApplicationBuilder(Cfg.class).run();
-
-        assertThatCode(() -> ctx.getBean(OrderedMetricCollectingServerInterceptor.class))
-                .isInstanceOf(NoSuchBeanDefinitionException.class);
-        assertThatCode(() -> ctx.getBean(OrderedMetricCollectingClientInterceptor.class))
-                .doesNotThrowAnyException();
-
-        ctx.close();
+        try (var ctx = new SpringApplicationBuilder(Cfg.class).run()) {
+            assertThatCode(() -> ctx.getBean(OrderedMetricCollectingServerInterceptor.class))
+                    .isInstanceOf(NoSuchBeanDefinitionException.class);
+            assertThatCode(() -> ctx.getBean(OrderedMetricCollectingClientInterceptor.class))
+                    .doesNotThrowAnyException();
+        }
     }
 
     @Test
     @Classpath(exclude = "grpc-client-boot-autoconfigure-*.jar")
     void testMetricsBeans_whenGrpcClientNotOnClasspath() {
-        ConfigurableApplicationContext ctx = new SpringApplicationBuilder(Cfg.class)
+        try (var ctx = new SpringApplicationBuilder(Cfg.class)
                 .properties(GrpcServerProperties.PREFIX + ".in-process.name=" + UUID.randomUUID())
-                .run();
+                .run()) {
 
-        assertThatCode(() -> ctx.getBean(OrderedMetricCollectingServerInterceptor.class))
-                .doesNotThrowAnyException();
-        assertThatCode(() -> ctx.getBean(OrderedMetricCollectingClientInterceptor.class))
-                .isInstanceOf(NoSuchBeanDefinitionException.class);
-
-        ctx.close();
+            assertThatCode(() -> ctx.getBean(OrderedMetricCollectingServerInterceptor.class))
+                    .doesNotThrowAnyException();
+            assertThatCode(() -> ctx.getBean(OrderedMetricCollectingClientInterceptor.class))
+                    .isInstanceOf(NoSuchBeanDefinitionException.class);
+        }
     }
 
     @Configuration(proxyBeanMethods = false)
