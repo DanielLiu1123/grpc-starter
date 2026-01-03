8b10c5c035c84c7824a4e8143cd62078e2f6231e
Freeman Liu <llw599502537@gmail.com>
2026-01-03 22:49:17 +0800

    feat: add support for gRPC metrics and tracing auto-configuration with Micrometer

---
+++ grpc-boot-autoconfigure/grpc-server-boot-autoconfigure/src/main/java/grpcstarter/server/feature/metrics/GrpcServerMetricsAutoConfiguration.java
@@ -0,0 +1,34 @@
+package grpcstarter.server.feature.metrics;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.grpc.MetricCollectingServerInterceptor;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.micrometer.metrics.autoconfigure.CompositeMeterRegistryAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+
+/**
+ * Auto-configuration for gRPC metrics with Micrometer.
+ *
+ * @author Freeman
+ */
+@AutoConfiguration(after = CompositeMeterRegistryAutoConfiguration.class)
+@ConditionalOnClass(MeterRegistry.class)
+@ConditionalOnBean(MeterRegistry.class)
+public class GrpcServerMetricsAutoConfiguration {
+
+    /**
+     * A relatively high priority.
+     */
+    public static final int ORDER = -10000;
+
+    @Bean
+    @Order(ORDER)
+    @ConditionalOnMissingBean(MetricCollectingServerInterceptor.class)
+    public MetricCollectingServerInterceptor metricCollectingServerInterceptor(MeterRegistry registry) {
+        return new MetricCollectingServerInterceptor(registry);
+    }
+}
---
+++ grpc-boot-autoconfigure/grpc-server-boot-autoconfigure/src/main/java/grpcstarter/server/feature/trace/GrpcServerTraceAutoConfiguration.java
@@ -0,0 +1,35 @@
+package grpcstarter.server.feature.trace;
+
+import io.micrometer.core.instrument.binder.grpc.ObservationGrpcServerInterceptor;
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.tracing.Tracer;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.micrometer.observation.autoconfigure.ObservationAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+
+/**
+ * Auto-configuration for gRPC tracing with Micrometer Observation.
+ *
+ * @author Freeman
+ */
+@AutoConfiguration(after = ObservationAutoConfiguration.class)
+@ConditionalOnClass(Tracer.class)
+@ConditionalOnBean(ObservationRegistry.class)
+public class GrpcServerTraceAutoConfiguration {
+
+    /**
+     * A relatively high priority.
+     */
+    public static final int ORDER = -10000;
+
+    @Bean
+    @Order(ORDER)
+    @ConditionalOnMissingBean(ObservationGrpcServerInterceptor.class)
+    public ObservationGrpcServerInterceptor observationGrpcServerInterceptor(ObservationRegistry registry) {
+        return new ObservationGrpcServerInterceptor(registry);
+    }
+}
