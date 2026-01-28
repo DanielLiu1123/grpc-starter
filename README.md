3133822f01e29c2c324fb46501e6d5ebf3e33a4f
Freeman Liu <llw599502537@gmail.com>
2026-01-29 01:13:48 +0800

    refactor: move metrics and tracing extensions into core autoconfigure modules (#143)
    * refactor: merge some extensions to autoconfigure module
    
    * feat: add support for gRPC metrics and tracing auto-configuration with Micrometer
    
    * refactor: remove grpc-metrics and grpc-tracing extensions, integrate functionality into grpc-boot-autoconfigure module
    
    * fmt
    
    * feat: add configurable tags for metrics and observations in example applications
    
    * update docs

--- README.md
+++ README.md
@@ -55,13 +55,12 @@ Refer to [quick-start](examples/quick-start).
 - gRPC client autoconfiguration
   - [`@Autowired` support](https://danielliu1123.github.io/grpc-starter/docs/client/autoconfiguration#inject-client)
   - [Dynamic refreshing](https://danielliu1123.github.io/grpc-starter/docs/client/dynamic-refresh)
+- Metrics & Tracing support via [Micrometer](https://micrometer.io/)
 
 ### Extensions:
 
 - [gRPC HTTP transcoding](https://danielliu1123.github.io/grpc-starter/docs/extensions/grpc-http-transcoding): Support both gRPC and HTTP/JSON with a single codebase, with optional OpenAPI integration.
 - [Protobuf validation](https://danielliu1123.github.io/grpc-starter/docs/extensions/protobuf-validation): Protobuf message validation using [protovalidate](https://github.com/bufbuild/protovalidate-java) and [protoc-gen-validate](https://github.com/bufbuild/protoc-gen-validate).
-- [Metrics](https://danielliu1123.github.io/grpc-starter/docs/extensions/metrics): Integrate Spring Boot Actuator with gRPC services.
-- [Tracing](https://danielliu1123.github.io/grpc-starter/docs/extensions/tracing): Integrate Spring Boot Actuator with gRPC server and client.
 - [Testing](https://danielliu1123.github.io/grpc-starter/docs/extensions/test): Integration with `SpringBootTest`.
 
 ## Documentation
---
+++ grpc-boot-autoconfigure/grpc-client-boot-autoconfigure/src/main/java/grpcstarter/client/feature/trace/GrpcClientTraceAutoConfiguration.java
@@ -0,0 +1,36 @@
+package grpcstarter.client.feature.trace;
+
+import grpcstarter.client.ConditionOnGrpcClientEnabled;
+import io.micrometer.core.instrument.binder.grpc.ObservationGrpcClientInterceptor;
+import io.micrometer.observation.ObservationRegistry;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.micrometer.observation.autoconfigure.ObservationAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+
+/**
+ * Auto-configuration for gRPC tracing with Micrometer Observation.
+ *
+ * @author Freeman
+ */
+@AutoConfiguration(after = ObservationAutoConfiguration.class)
+@ConditionOnGrpcClientEnabled
+@ConditionalOnClass(ObservationRegistry.class)
+@ConditionalOnBean(ObservationRegistry.class)
+public class GrpcClientTraceAutoConfiguration {
+
+    /**
+     * A relatively high priority.
+     */
+    public static final int ORDER = -10000;
+
+    @Bean
+    @Order(ORDER)
+    @ConditionalOnMissingBean(ObservationGrpcClientInterceptor.class)
+    public ObservationGrpcClientInterceptor observationGrpcClientInterceptor(ObservationRegistry registry) {
+        return new ObservationGrpcClientInterceptor(registry);
+    }
+}
