bcdbf511a22920c341c672801539d821c03a7a3c
Freeman Liu <llw599502537@gmail.com>
2026-06-11 15:46:12 +0800

    update protobugVersion to 4.34.2

--- grpc-boot-autoconfigure/grpc-client-boot-autoconfigure/src/test/java/grpcstarter/client/MultipleMatchedChannelsIT.java
+++ grpc-boot-autoconfigure/grpc-client-boot-autoconfigure/src/test/java/grpcstarter/client/MultipleMatchedChannelsIT.java
@@ -10,6 +10,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.test.util.TestSocketUtils;
 
 /**
  * @author Freeman
@@ -19,23 +20,28 @@ class MultipleMatchedChannelsIT {
 
     @Test
     void whenMatchedMultipleChannels_shouldUseFirstOneAndLogWarning(CapturedOutput output) {
+        var serverPort = TestSocketUtils.findAvailableTcpPort();
+        var chan1Port = serverPort + 1;
+        var chan2Port = serverPort + 2;
+        var chan3Port = serverPort + 3;
         try (var ctx = new SpringApplicationBuilder(Cfg.class)
+                .properties("grpc.server.port=" + serverPort)
                 .properties("grpc.client.base-packages[0]=io.grpc")
                 .properties("grpc.client.channels[0].name=ch1")
-                .properties("grpc.client.channels[0].authority=localhost:9091")
+                .properties("grpc.client.channels[0].authority=localhost:" + chan1Port)
                 .properties("grpc.client.channels[0].classes[0]=io.grpc.health.v1.HealthGrpc$HealthBlockingStub")
                 .properties("grpc.client.channels[1].name=ch2")
-                .properties("grpc.client.channels[1].authority=localhost:9092")
+                .properties("grpc.client.channels[1].authority=localhost:" + chan2Port)
                 .properties("grpc.client.channels[1].stubs[0]=io.grpc.health.v1.HealthGrpc$HealthBlockingStub")
                 .properties("grpc.client.channels[2].name=ch3")
-                .properties("grpc.client.channels[2].authority=localhost:9093")
+                .properties("grpc.client.channels[2].authority=localhost:" + chan3Port)
                 .properties("grpc.client.channels[2].services[0]=grpc.health.v1.Health")
                 .run()) {
 
             HealthGrpc.HealthBlockingStub stub = ctx.getBean(HealthGrpc.HealthBlockingStub.class);
 
             // Channel should be the first matched one (ch1)
-            assertThat(stub.getChannel().authority()).isEqualTo("localhost:9091");
+            assertThat(stub.getChannel().authority()).isEqualTo("localhost:" + chan1Port);
 
             assertThat(output.getOut())
                     .contains(
